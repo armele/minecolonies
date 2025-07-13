87951f5867736234c1b04c512f1d30ac16beaaf7
Al Mele <amele@carolina.rr.com>
2025-07-13 07:13:21 -0400

    Update CitizenFoodHandler.java

--- src/main/java/com/minecolonies/core/entity/citizen/citizenhandlers/CitizenFoodHandler.java
+++ src/main/java/com/minecolonies/core/entity/citizen/citizenhandlers/CitizenFoodHandler.java
@@ -164,7 +164,7 @@ public class CitizenFoodHandler implements ICitizenFoodHandler
         return baseModifier * 0.5 * Math.min(2.5, 5.0/getFoodHappinessStats().diversity());
     }
 
-    
+    @Override
     public ImmutableList<Item> getLastEatenFoods()
     {
         return ImmutableList.copyOf(lastEatenFoods);
