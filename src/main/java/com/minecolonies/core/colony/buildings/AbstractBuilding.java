c822c658f3b44d9535067e074b6051069432e950
Al Mele <amele@carolina.rr.com>
2025-11-02 11:15:32 -0500

    Safeguard NPE (#11292)

--- src/main/java/com/minecolonies/core/colony/buildings/AbstractBuilding.java
+++ src/main/java/com/minecolonies/core/colony/buildings/AbstractBuilding.java
@@ -1768,7 +1768,8 @@ public abstract class AbstractBuilding extends AbstractBuildingContainer
 
         for (final IToken<?> child : target.getChildren())
         {
-            if (isRequestStuck(colony.getRequestManager().getRequestForToken(child), playerResolverRequests, retryingRequests))
+            final IRequest<?> request = colony.getRequestManager().getRequestForToken(child);
+            if (request != null && isRequestStuck(request, playerResolverRequests, retryingRequests))
             {
                 return true;
             }
